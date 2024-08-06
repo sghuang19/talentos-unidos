8d112d9110fb23f2345f586d6b1adb47f6b55629
Samuel Huang <hi@sghuang.com>
2024-08-06 15:09:31 -0400

    Add Mongoose into dependencies

--- UserInfo.js
+++ UserInfo.js
@@ -1,3 +1,6 @@
+import mongoose, { Schema } from "mongoose";
+import "dotenv/config";
+
 export function createUserInfo(phoneNumber) {
   return {
     phoneNumber: phoneNumber,
@@ -11,3 +14,23 @@ export function createUserInfo(phoneNumber) {
     resume: null,
   };
 }
+
+const userInfoSchema = new mongoose.Schema({
+  phoneNumber: String,
+  firstname: String,
+  lastname: String,
+  birthdate: Date,
+  english: Number,
+  permit: Boolean,
+  zipcode: String,
+  relocate: Boolean,
+  resume: {
+    filename: String,
+    contentType: String,
+    id: Schema.Types.ObjectId,
+  },
+});
+
+// await mongoose.connect(process.env.MONGODB_URI);
+
+export const UserInfo = mongoose.model("UserInfo", userInfoSchema);
